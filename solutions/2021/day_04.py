dc393907fddce65a534a8a5f003f35264e4f20a3
Luke Spademan <info@lukespademan.com>
2021-12-04 12:32:30 +0000

    Solved 2021 day 4

---
+++ solutions/2021/day_04.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python
+
+"""
+Puzzle Title:     AoC 2021 Day 4: Giant Squid
+Puzzle Link:      https://adventofcode.com/2021/day/4
+Solution Author:  Luke Spademan <info@lukespademan.com>
+Solution License: MIT
+"""
+
+import fileinput
+from dataclasses import dataclass
+import copy
+
+
+@dataclass
+class Square:
+    value: int
+    called: bool = False
+
+
+def parse_input():
+    data = []
+    grids = []
+    grid = []
+    for i, line in enumerate(fileinput.input()):
+        line = line.rstrip()
+        if i == 0:
+            call_order = [int(num) for num in line.split(",")]
+            continue
+        if line:
+            row = [Square(int(num)) for num in line.split(" ") if num]
+            grid.append(row)
+        else:
+            if grid:
+                grids.append(grid)
+                grid = []
+    return call_order, grids
+
+
+def get_bingo(call_order, grids):
+    for num in call_order:
+        for grid in grids:
+            cols_all_called = [True for _ in range(5)]
+            for row in grid:
+                all_called = True
+                for col, item in enumerate(row):
+                    if item.value == num:
+                        item.called = True
+                    if item.called == False:
+                        all_called = False
+                        cols_all_called[col] = False
+                if all_called:
+                    return grid, num
+            if sum(cols_all_called) > 0:
+                return grid, num
+
+
+def last_winner(call_order, grids):
+    winners = []
+    for num in call_order:
+        for i, grid in enumerate(grids):
+            cols_all_called = [True for _ in range(5)]
+            for row in grid:
+                all_called = True
+                for col, item in enumerate(row):
+                    if item.value == num:
+                        item.called = True
+                    if item.called == False:
+                        all_called = False
+                        cols_all_called[col] = False
+                if all_called:
+                    if i not in winners:
+                        winners.append(i)
+                        if len(winners) == len(grids):
+                            return grid, num
+            if sum(cols_all_called) > 0:
+                if i not in winners:
+                    winners.append(i)
+                    if len(winners) == len(grids):
+                        return grid, num
+    return grid, num
+
+
+def calculate_score(grid, last_called):
+    score = 0
+    for row in grid:
+        for item in row:
+            if not item.called:
+                score += item.value
+    score *= last_called
+    return score
+
+
+def solve_part1(call_order, grids):
+    winning_grid, last_num = get_bingo(call_order, grids)
+    return calculate_score(winning_grid, last_num)
+
+
+def solve_part2(call_order, grids):
+    losing_grid, last_num = last_winner(call_order, grids)
+    return calculate_score(losing_grid, last_num)
+
+
+def main():
+    call_order, grids = parse_input()
+    grids_p1 = copy.deepcopy(grids)
+    grids_p2 = copy.deepcopy(grids)
+
+    part1_ans = solve_part1(call_order, grids_p1)
+    print(f"Part 1: {part1_ans}")
+
+    part2_ans = solve_part2(call_order, grids_p2)
+    print(f"Part 2: {part2_ans}")
+
+
+if __name__ == "__main__":
+    main()
