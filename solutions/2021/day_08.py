20378cfd25c9c678279e57f04e79a797aed865be
Luke Spademan <info@lukespademan.com>
2021-12-09 02:35:55 +0000

    Solved 2021 day 8

---
+++ solutions/2021/day_08.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python
+
+"""
+Puzzle Title:     AoC 2021 Day 8: Seven Segment Search
+Puzzle Link:      https://adventofcode.com/2021/day/8
+Solution Author:  Luke Spademan <info@lukespademan.com>
+Solution License: MIT
+"""
+
+import fileinput
+from collections import defaultdict
+
+"""
+0: 6
+6: 6
+9: 6
+
+1: 2
+
+4: 4
+
+7: 3
+
+8: 7
+
+2: 5
+3: 5
+5: 5
+
+  0:      1:      2:      3:      4:
+ aaaa    ....    aaaa    aaaa    ....
+b    c  .    c  .    c  .    c  b    c
+b    c  .    c  .    c  .    c  b    c
+ ....    ....    dddd    dddd    dddd
+e    f  .    f  e    .  .    f  .    f
+e    f  .    f  e    .  .    f  .    f
+ gggg    ....    gggg    gggg    ....
+
+  5:      6:      7:      8:      9:
+ aaaa    aaaa    aaaa    aaaa    aaaa
+b    .  b    .  .    c  b    c  b    c
+b    .  b    .  .    c  b    c  b    c
+ dddd    dddd    ....    dddd    dddd
+.    f  e    f  .    f  e    f  .    f
+.    f  e    f  .    f  e    f  .    f
+ gggg    gggg    ....    gggg    gggg
+"""
+
+NUMS = {
+    "0": {"a", "b", "c", "e", "f", "g"},
+    "1": {"c", "f"},
+    "2": {"a", "c", "d", "e", "g"},
+    "3": {"a", "c", "d", "f", "g"},
+    "4": {"b", "c", "d", "f"},
+    "5": {"a", "b", "d", "f", "g"},
+    "6": {"a", "b", "d", "e", "f", "g"},
+    "7": {"a", "c", "f"},
+    "8": {"a", "b", "c", "d", "e", "f", "g"},
+    "9": {"a", "b", "c", "d", "f", "g"},
+}
+
+
+def parse_input():
+    data = []
+    for line in map(lambda l: l.rstrip(), fileinput.input()):
+        if line:
+            training, test = line.split(" | ")
+            data.append(
+                {
+                    "training": [set(digit) for digit in training.split(" ")],
+                    "test": [set(digit) for digit in test.split(" ")],
+                }
+            )
+    return data
+
+
+def solve_part1(data):
+    total = 0
+    for row in data:
+        for item in row["test"]:
+            match len(item):
+                case 2:
+                    total += 1
+                case 4:
+                    total += 1
+                case 3:
+                    total += 1
+                case 7:
+                    total += 1
+    return total
+
+
+
+def solve_part2(data):
+    total = 0
+    for row in data:
+        digits = {}
+        for item in row["training"]:
+            match len(item):
+                case 2:
+                    digits["1"] = item
+                case 4:
+                    digits["4"] = item
+                case 3:
+                    digits["7"] = item
+                case 7:
+                    digits["8"] = item
+        num = 0
+        for item in row["test"]:
+            num *= 10
+            match len(item):
+                case 2:
+                    num += 1
+                case 4:
+                    num += 4
+                case 3:
+                    num += 7
+                case 7:
+                    num += 8
+                case 6:
+                    if not digits["1"].issubset(item):
+                        num += 6
+                    elif digits["4"].issubset(item):
+                        num += 9
+                    else:
+                        digits["0"] = item
+                case 5:
+                    if digits["1"].issubset(item):
+                        num += 3
+                    elif (digits["4"] - digits["1"]).issubset(item):
+                        num += 5
+                    else:
+                        num += 2
+        total += num
+    return total
+
+
+def main():
+    data = parse_input()
+
+    part1_ans = solve_part1(data)
+    print(f"Part 1: {part1_ans}")
+
+    part2_ans = solve_part2(data)
+    print(f"Part 2: {part2_ans}")
+
+
+if __name__ == "__main__":
+    main()
